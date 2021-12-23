9974a080a4ff77881647ec592fe33ccf9147f532
Habib Siddiqui <habib.my21@gmail.com>
2021-12-23 14:03:52 +0530

    Added Docs

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const url = 'mongodb+srv://habib:siddiqui@employee-data.pnjdz.mongodb.net/Employee-Data?retryWrites=true&w=majority';
-const serverport = 5000;
 
-module.exports = { url, serverport }
+module.exports = { url }
 
